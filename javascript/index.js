fed9348971e96dc396087214682ab357e463609b
Jess Pates <jesspates@hotmail.co.uk>
2023-09-03 09:53:32 +0100

    navbar scroll to sections js

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,32 @@
 const navMenuButton = document.querySelector(".nav-menu__button");
 const navMenuContainer = document.querySelector(".burger__menu--container");
+const navMenu = document.querySelector('.burger__menu');
 const navMenuCloseButton = document.querySelector(".burger__menu--close");
+const homeSection = document.querySelector("#home--section");
+const navLogo = document.querySelector(".nav-logo");
 
 navMenuButton.addEventListener("click", function() {
+  console.log("test");
   navMenuContainer.classList.add("open");
-  // navMenuContainer.style.display = "block";
-  // navMenuContainer.classList.remove("hidden");
 })
 
 navMenuCloseButton.addEventListener("click", function() {
-  // navMenuContainer.style.display = "none";
   navMenuContainer.classList.remove("open");
+})
 
-  // navMenuContainer.classList.remove("hidden");
+// Navbar links
+navMenu.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('burger__menu--item')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+  navMenuContainer.classList.remove("open");
+});
+
+navLogo.addEventListener("click", function(e) {
+  e.preventDefault();
+  console.log("logo test");
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 })
